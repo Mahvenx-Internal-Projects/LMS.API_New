b15d057a907457ede4cacc809d060a79f65353ee
madicharla someswarao <somu.eswar8@outlook.com>
2026-07-29 11:21:28 +0530

    #code changes

---
+++ Migrations/20260728171836_AddBenchDocumentFields.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBenchDocumentFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddlID",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Address1",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Address2",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Comments",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "HireDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LangID",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LearningID",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Mobile",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PIN",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefBatchID",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RegBy",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AadhaarKey",
+                table: "BenchResources",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AadhaarNumber",
+                table: "BenchResources",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AadhaarUrl",
+                table: "BenchResources",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtherDocumentsJson",
+                table: "BenchResources",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PanCardKey",
+                table: "BenchResources",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PanCardNumber",
+                table: "BenchResources",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PanCardUrl",
+                table: "BenchResources",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePhotoKey",
+                table: "BenchResources",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePhotoUrl",
+                table: "BenchResources",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResumeFileName",
+                table: "BenchResources",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResumeKey",
+                table: "BenchResources",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResumeUrl",
+                table: "BenchResources",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AadhaarKey",
+                table: "BenchResources");
+
+            migrationBuilder.DropColumn(
+                name: "AadhaarNumber",
+                table: "BenchResources");
+
+            migrationBuilder.DropColumn(
+                name: "AadhaarUrl",
+                table: "BenchResources");
+
+            migrationBuilder.DropColumn(
+                name: "OtherDocumentsJson",
+                table: "BenchResources");
+
+            migrationBuilder.DropColumn(
+                name: "PanCardKey",
+                table: "BenchResources");
+
+            migrationBuilder.DropColumn(
+                name: "PanCardNumber",
+                table: "BenchResources");
+
+            migrationBuilder.DropColumn(
+                name: "PanCardUrl",
+                table: "BenchResources");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePhotoKey",
+                table: "BenchResources");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePhotoUrl",
+                table: "BenchResources");
+
+            migrationBuilder.DropColumn(
+                name: "ResumeFileName",
+                table: "BenchResources");
+
+            migrationBuilder.DropColumn(
+                name: "ResumeKey",
+                table: "BenchResources");
+
+            migrationBuilder.DropColumn(
+                name: "ResumeUrl",
+                table: "BenchResources");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddlID",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address1",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address2",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Comments",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "HireDate",
+                table: "Users",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LangID",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LearningID",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Mobile",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PIN",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RefBatchID",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegBy",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
